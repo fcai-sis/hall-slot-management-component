ee9e5ddee3df48295c0751074e8d7ac09357345e
Crispy <b1063629@gmail.com>
2024-01-25 20:15:41 +0200

    Refactor get.all.slot.handler.ts to implement pagination

--- src/features/example/logic/handlers/get.all.slot.handler.ts
+++ src/features/example/logic/handlers/get.all.slot.handler.ts
@@ -5,8 +5,22 @@ import Slot from "../../data/models/halls.model";
  * A handler that creates a new hall document in the database
  */
 
+type HandlerRequest = Request<
+  {},
+  {},
+  {
+    page: number;
+    pageSize: number;
+  }
+>;
+
 const handler = async (req: Request, res: Response) => {
-  const slots = await Slot.find();
+  const page = req.body.page;
+  const pageSize = req.body.pageSize;
+
+  const slots = await Slot.find()
+    .skip((page - 1) * pageSize)
+    .limit(pageSize);
   res.status(200).json({ slots });
 };
 
