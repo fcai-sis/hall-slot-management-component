aa834db36ef2035a89171a4514c6bc0f094950c7
Crispy <b1063629@gmail.com>
2024-01-25 11:18:47 +0200

    Refactor validateExampleMessageMiddleware to use ValidateHallData middleware

--- src/features/example/slot.routes.ts
+++ src/features/example/slot.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import asyncHandler from "../../core/asyncHandler";
-import validateExampleMessageMiddleware from "./logic/middlewares/validateHallData.middleware";
+import { ValidateHallData } from "./logic/middlewares/validateHallData.middleware";
 import createSlotsHandler from "./logic/handlers/create.slot.handler";
 import getAllSlotsHandler from "./logic/handlers/get.all.slot.handler";
 import getSlotByIdHandler from "./logic/handlers/get.slot.by.id.handler";
@@ -13,7 +13,7 @@ export default (router: Router) => {
     "/slots",
 
     // Validate example message
-    validateExampleMessageMiddleware,
+    ValidateHallData,
 
     // Handle example request
     asyncHandler(createSlotsHandler)
