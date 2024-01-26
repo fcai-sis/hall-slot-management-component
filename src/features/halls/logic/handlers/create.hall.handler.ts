76e62ca97d4ae0c9e80d99832c84b47ad7a3f4de
Crispy <b1063629@gmail.com>
2024-01-26 10:19:24 +0200

    Fix typo in handlerRequest type definition (But i dont know how to use it)

--- src/features/halls/logic/handlers/create.hall.handler.ts
+++ src/features/halls/logic/handlers/create.hall.handler.ts
@@ -5,7 +5,7 @@ import Hall from "../../data/models/halls.model";
  * A handler that creates a new hall document in the database
  */
 
-type handlerRequest = Request<{}, {}, { name: string; capacity: number }>;
+type HandlerRequest = Request<{}, {}, { name: string; capacity: number }>;
 
 const handler = async (req: Request, res: Response) => {
   const hall = await Hall.create(req.body);
