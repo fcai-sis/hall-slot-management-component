e495ded8af7f016a7c92c178260746d9e289acf9
Crispy <b1063629@gmail.com>
2024-01-27 13:24:44 +0200

    Refactor hall data validation middleware

--- src/features/halls/logic/middlewares/validateHallData.middleware.ts
+++ src/features/halls/logic/middlewares/validateHallData.middleware.ts
@@ -1,18 +1,39 @@
 import { NextFunction, Request, Response } from "express";
-import * as validator from "express-validator";
+import { body, validationResult } from "express-validator";
 
-export const validateHallData = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { name, capacity } = req.params;
+const middlewares = [
+  // Validation middleware using express-validator for hallId
+  body("name")
+    .exists()
+    .withMessage("name is required")
+    .isString()
+    .withMessage("name must be a valid String"),
 
-  if (!name || !capacity) {
-    return res.status(400).json({
-      message: "Invalid Hall Data",
-    });
-  }
+  body("capacity")
+    .exists()
+    .withMessage("capacity is required")
+    .isInt({ gt: 0 })
+    .withMessage("capacity must be a valid Integer"),
 
-  next();
-};
+  // Middleware to handle validation errors
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: {
+          message: errors.array()[0].msg,
+        },
+      });
+    }
+
+    // Trim the name if needed
+    req.body.name = req.body.name.trim();
+
+    // Proceed to the next middleware or route handler
+    next();
+  },
+];
+
+const validateHallData = middlewares;
+export default validateHallData;
