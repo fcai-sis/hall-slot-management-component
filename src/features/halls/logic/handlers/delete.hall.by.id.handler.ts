2cec2cfcbdec048f37f26c4fff259a15aa4d5db1
Crispy <b1063629@gmail.com>
2024-01-27 13:23:49 +0200

    Refactor delete hall handler to include a response message

--- src/features/halls/logic/handlers/delete.hall.by.id.handler.ts
+++ src/features/halls/logic/handlers/delete.hall.by.id.handler.ts
@@ -7,14 +7,17 @@ import Hall from "../../data/models/halls.model";
  * A handler that deletes a hall document from the database
  */
 
-const handler = async (req: Request, res: Response) => {
+type HandlerRequest = Request<{ id: string }>;
+
+const handler = async (req: HandlerRequest, res: Response) => {
   const hall = await Hall.findByIdAndDelete(req.params.id);
 
   if (!hall) {
     return res.status(404).json({ error: "Hall not found" });
   }
 
-  res.status(204).end(); // 204 means no content
+  // Werid the message is not showing
+  res.status(204).json({ message: "Hall Deleted successfully" }); // 204 means no content
 };
 
 export default handler;
