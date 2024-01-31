96abc897bcf9b4a9b79c6568bc71e1de015da0c2
Amr Khaled <lewolfje@gmail.com>
2024-01-31 14:48:47 +0200

    Refactor delete.slot.by.id.handler.ts to update error response

--- src/features/slots/logic/handlers/delete.slot.by.id.handler.ts
+++ src/features/slots/logic/handlers/delete.slot.by.id.handler.ts
@@ -1,10 +1,10 @@
-// logic for deleting a hall from the database
+// logic for deleting a slot from the database
 
 import { Request, Response } from "express";
 import Slot from "../../data/models/slot.model";
 
 /**
- * A handler that deletes a hall document from the database
+ * A handler that deletes a slot document from the database
  */
 
 type HandlerRequest = Request<{ id: string }>;
@@ -13,9 +13,13 @@ const handler = async (req: HandlerRequest, res: Response) => {
   const slot = await Slot.findByIdAndDelete(req.params.id);
 
   if (!slot) {
-    return res.status(404).json({ error: "Slot not found" });
+    return res.status(404).json({
+      error: {
+        message: "Slot not found",
+      },
+    });
   }
-  res.status(200).json({ message: "Slot Deleted successfully " }); // 204 means no content
+  return res.status(200).json({ message: "Slot Deleted successfully " }); // 204 means no content
 };
 
 export default handler;
